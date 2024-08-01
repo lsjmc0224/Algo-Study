2e1693d1f31a9b5c2c043cc5493ba4a8d3e17d77
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2024-08-02 01:07:15 +0900

    [Gold V] Title: 적록색약, Time: 176 ms, Memory: 111704 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,36 +1,44 @@
-import sys
-sys.setrecursionlimit(10000)
-
 def solve(arr):
-    def dfs(ci, cj):
-        v[ci][cj] = 1
+    def bfs(si, sj):
+        q = []
 
-        for di, dj in dir:
-            ni, nj = ci + di, cj + dj
-            if 0 <= ni < N and 0 <= nj < N and v[ni][nj] == 0 and arr[ci][cj] == arr[ni][nj]:
-                dfs(ni, nj)
+        q.append((si, sj))
+        v[si][sj] = 1
 
-    v = [[0]*N for _ in range(N)]
+        while q:
+            ci, cj = q.pop(0)
+
+            for di, dj in dir:
+                ni, nj = ci+di, cj+dj
+                if 0<=ni<N and 0<=nj<N and v[ni][nj] == 0 and arr[ni][nj] == arr[ci][cj]:
+                    v[ni][nj] = 1
+                    q.append((ni, nj))
+
+    v = [[0] * N for _ in range(N)]
     cnt = 0
 
     for i in range(N):
         for j in range(N):
             if v[i][j] == 0:
-                dfs(i, j)
+                bfs(i, j)
                 cnt += 1
     return cnt
 
 
 N = int(input())
 pic = [list(map(str, input().strip())) for _ in range(N)]
-fake = [[0] * N for _ in range(N)]
-dir = [(1,0), (-1,0), (0,1), (0,-1)]
+fake = [[0]*N for _ in range(N)]
+dir = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
 
 for i in range(N):
     for j in range(N):
-        if pic[i][j] == 'R' or pic[i][j] == 'G':
+        if pic[i][j] == 'R' or pic[i][j] == 'G':  # 적녹은 0으로, 청은 1로 fake를 채움
             fake[i][j] = 0
         elif pic[i][j] == 'B':
             fake[i][j] = 1
 
-print(solve(pic), solve(fake))
+ans = solve(pic)
+ans_ = solve(fake)
+
+print(ans, ans_)
