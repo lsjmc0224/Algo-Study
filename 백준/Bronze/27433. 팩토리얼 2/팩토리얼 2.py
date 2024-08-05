070ca3967a2032442abbf217210087e1fda87507
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2024-08-05 13:21:01 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
@@ -1,3 +1,13 @@
-import math 
-n = int(input())
-print(math.factorial(n))
+def solve(n):
+    global ans
+
+    if n > N:
+        return
+    solve(n+1)
+    ans *= n
+
+
+N = int(input())
+ans = 1
+solve(1)
+print(ans)
