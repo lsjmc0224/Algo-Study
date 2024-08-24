678e76aa32d61f4e1c8dd6a48e21544a27c90b6a
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2024-08-24 16:50:47 +0900

    [Bronze III] Title: 전자레인지, Time: 100 ms, Memory: 108080 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.py
+++ 백준/Bronze/10162. 전자레인지/전자레인지.py
@@ -1,14 +1,15 @@
 T = int(input())
 temp = T
 
-minute = temp // 60
-temp -= minute * 60
+A = temp // 300
+temp %= 300
+
+B = temp // 60
+temp %= 60
+
 C = temp // 10
+temp %= 10
 
-A = minute // 5
-B = minute - A
-
-if (A*300) + (B*60) + (C*10) != T:
+if temp != 0:
     print(-1)
-
 else: print(A, B, C)
