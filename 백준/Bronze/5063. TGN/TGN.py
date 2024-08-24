9d8bf3241af040e57cdf36be5a7708acb466bfed
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2024-08-24 16:14:33 +0900

    [Bronze III] Title: TGN, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for _ in range(N):
+    r, e, c = map(int, input().split())
+
+    if r > (e-c):
+        print("do not advertise")
+    elif r < (e-c):
+        print("advertise")
+    else:
+        print("does not matter")
