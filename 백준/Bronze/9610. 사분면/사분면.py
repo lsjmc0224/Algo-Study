87ee976162b2db6c0e5074964a42511e977a2d26
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2024-08-24 16:28:10 +0900

    [Bronze III] Title: 사분면, Time: 100 ms, Memory: 110272 KB -BaekjoonHub

---
+++ 백준/Bronze/9610. 사분면/사분면.py
@@ -0,0 +1,20 @@
+N = int(input())
+Q1, Q2, Q3, Q4 = 0, 0, 0, 0
+AXIS = 0
+for _ in range(N):
+    x, y = map(int, input().split())
+
+    if x == 0 or y == 0:
+        AXIS += 1
+    elif x > 0:
+        if y > 0: Q1 += 1
+        else: Q4 += 1
+    elif x < 0:
+        if y > 0: Q2 += 1
+        else: Q3 += 1
+
+print(f"Q1: {Q1}")
+print(f"Q2: {Q2}")
+print(f"Q3: {Q3}")
+print(f"Q4: {Q4}")
+print(f"AXIS: {AXIS}")
